96b9a7dd9fb6e279ee373b5350afb8dd85d523c5
Irene Njeru <irenenjeru8@gmail.com>
2022-07-21 13:35:29 +0300

    task completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,14 @@
 class BirdsController < ApplicationController
+# UPDATE /birds
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
 
   # GET /birds
   def index
@@ -28,4 +38,14 @@ class BirdsController < ApplicationController
     params.permit(:name, :species)
   end
 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+  
 end
